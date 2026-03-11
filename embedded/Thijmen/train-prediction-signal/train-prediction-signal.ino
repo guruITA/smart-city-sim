13a016d2a74a59414dcc849ea780ca6c9fd1cf93
WalterNoot <thijmen.walter@hva.nl>
2026-03-11 18:08:10 +0100

    feat: :sparkles: added servo barrier and removed oled screen

--- embedded/Thijmen/train-prediction-signal/train-prediction-signal.ino
+++ embedded/Thijmen/train-prediction-signal/train-prediction-signal.ino
@@ -1,12 +1,12 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
+#include <ESP32Servo.h>
 
 // Output LED pins (warning lights)
 #define LED_1 37
 #define LED_2 36
+
+// Servo pin
+#define SERVO_PIN 18
 
 // Input button pin
 #define BTN 45
@@ -15,15 +15,11 @@
 #define A_B_DISTANCE 200
 #define B_C_DISTANCE 1000
 
-// Oled screen pins
-#define SCL_PIN 13
-#define SDA_PIN 14
-
 // Oled screen width and height
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 enum State {
   IDLE,
@@ -32,6 +28,8 @@ enum State {
 };
 
 State currentState = IDLE;
+
+Servo servo;
 
 const unsigned int safetyMargin = 5000;
 
@@ -44,13 +42,35 @@ bool lastReading = HIGH;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
-unsigned long lastDisplayUpdate = 0;
-const unsigned long displayInterval = 100;
+// unsigned long lastDisplayUpdate = 0;
+// const unsigned long displayInterval = 100;
 
-// Warning light variables
 unsigned long blinkTimer = 0;
 const unsigned long blinkInterval = 400;
 bool ledToggle = false;
+
+const int BARRIER_OPEN = 0;
+const int BARRIER_CLOSED = 90;
+int currentBarrierPos = BARRIER_OPEN;
+
+void handleServo() {
+
+  if (currentState != WAITING) {
+    if (currentBarrierPos != BARRIER_OPEN) {
+      servo.write(BARRIER_OPEN);
+      currentBarrierPos = BARRIER_OPEN;
+    }
+    return;
+  }
+
+  unsigned long elapsed = millis() - startMillis;
+
+  if (elapsed >= predictedTime && currentBarrierPos != BARRIER_CLOSED) {
+    servo.write(BARRIER_CLOSED);
+    currentBarrierPos = BARRIER_CLOSED;
+  }
+
+}
 
 void handleWarningLEDS() {
 
@@ -117,70 +137,18 @@ void handleButton() {
   lastReading = reading;
 }
 
-void updateDisplay() {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-
-  switch (currentState) {
-
-    case IDLE:
-      display.println("IDLE");
-      break;
-
-    case MEASURING:
-      display.println("MEASURING");
-      display.setTextSize(1);
-      display.print("Elapsed: ");
-      display.print((millis() - startMillis) / 1000.0, 2);
-      display.println(" s");
-      break;
-
-    case WAITING:
-      display.println("WAITING");
-      display.setTextSize(1);
-      display.print("Predicted: ");
-      display.print(predictedTime / 1000.0, 2);
-      display.println(" s");
-
-      unsigned long remaining =
-        (millis() - startMillis >= predictedTime)
-        ? 0
-        : predictedTime - (millis() - startMillis);
-
-      display.print("Remaining: ");
-      display.print(remaining / 1000.0, 2);
-      display.println(" s");
-      break;
-  }
-
-  display.display();
-}
-
 void setup() {
   Serial.begin(115200);
-
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
   pinMode(BTN, INPUT_PULLUP);
-
-  Wire.begin(SDA_PIN, SCL_PIN);
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    for (;;);
-  }
-
-  display.clearDisplay();
-  display.display();
+	servo.attach(SERVO_PIN, 500, 2400);
+  servo.write(BARRIER_OPEN);
+  Serial.println("Setup complete");
 }
 
 void loop() {
   handleButton();
   handleWarningLEDS();
-
-  if (millis() - lastDisplayUpdate > displayInterval) {
-    lastDisplayUpdate = millis();
-    updateDisplay();
-  }
+  handleServo();
 }
