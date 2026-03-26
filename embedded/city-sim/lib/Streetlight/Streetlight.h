fb8ba33cd0915673ef9c0c7091d9db191dbfc496
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2026-03-26 11:22:42 +0100

    feat: implement StreetLight class with initialization and update logic for automatic street lighting

--- embedded/city-sim/lib/Streetlight/Streetlight.h
+++ embedded/city-sim/lib/Streetlight/Streetlight.h
@@ -1,1 +1,23 @@
-    
+#ifndef STREETLIGHT_H
+#define STREETLIGHT_H
+
+#include <Arduino.h>
+
+class StreetLight {
+
+private:
+  int _ldrPin;
+  int _relayPin;
+  int _threshold;
+
+  int _interval;
+  unsigned long _previousMillis;
+
+public:
+  StreetLight(int ldrPin, int relayPin, int threshold, int interval);
+
+  void begin();
+  void update();
+};
+
+#endif
