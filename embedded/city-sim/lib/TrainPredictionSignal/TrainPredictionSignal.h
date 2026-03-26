5ddb84c27b2239f280600060c69a06fa07fbfcfa
Thijmen <thijmen.walter@hva.nl>
2026-03-26 13:26:48 +0100

    feat: :sparkles: Made a library based on my train signal demo for implentation to a whole city file

---
+++ embedded/city-sim/lib/TrainPredictionSignal/TrainPredictionSignal.h
@@ -0,0 +1,69 @@
+#ifndef TRAIN_PREDICTION_SIGNAL_H
+#define TRAIN_PREDICTION_SIGNAL_H
+
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+class TrainPredictionSignal {
+private:
+  // Pins
+  int _led1Pin;
+  int _led2Pin;
+  int _buzzerPin;
+  int _servoPin;
+  int _btnPin;
+
+  // Distances
+  int _aBDistance;
+  int _bCDistance;
+
+  // Other parameters
+  unsigned int _safetyMargin;
+  int _buzzerFreq;
+  int _buzzerResolution;
+  int _barrierOpenAngle;
+  int _barrierClosedAngle;
+
+  // State
+  enum State {
+    IDLE,
+    MEASURING,
+    WAITING
+  };
+  State _currentState;
+
+  // Servo
+  Servo _servo;
+  int _currentBarrierPos;
+
+  // Timing
+  unsigned long _startMillis;
+  unsigned long _predictedTime;
+
+  // Button debouncing
+  bool _buttonState;
+  bool _lastReading;
+  unsigned long _lastDebounceTime;
+  const unsigned long _debounceDelay = 50;
+
+  // LED blinking
+  unsigned long _blinkTimer;
+  const unsigned long _blinkInterval = 400;
+  bool _ledToggle;
+
+  // Methods
+  void handleServo();
+  void handleWarningLEDSAndSound();
+  void handleButton();
+
+public:
+  TrainPredictionSignal(int led1Pin, int led2Pin, int buzzerPin, int servoPin, int btnPin,
+                        int aBDistance = 200, int bCDistance = 1000, unsigned int safetyMargin = 5000,
+                        int buzzerFreq = 1000, int buzzerResolution = 8,
+                        int barrierOpenAngle = 0, int barrierClosedAngle = 90);
+
+  void begin();
+  void update();
+};
+
+#endif
