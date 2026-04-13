f64bc7256f0ec065b786b7484dc4b53edc94edec
Gurpreet Singh <gurpreet.singh2@hva.nl>
2026-04-13 09:58:24 +0200

    docs: Done with advice goal

--- embedded/city-sim/city-sim.ino
+++ embedded/city-sim/city-sim.ino
@@ -2,12 +2,13 @@
 #include "Streetlight.h"
 #include "TrainPredictionSignal.h"
 #include "SpeedCamera.h"
+#include "SpeedCamera.h"
 
 #define builtin LED_BUILTIN
 
-// WiFi details
-const char* WIFI_SSID = "";
-const char* WIFI_PASSWORD = "";
+//  WiFi details
+const char* WIFI_SSID = "ESP32CAM_CAPTURE";
+const char* WIFI_PASSWORD = "12345678";
 
 // backend URL
 const String API_BASE_URL = "";
@@ -17,7 +18,6 @@ StreetLight lamp(4, 5, 650, 1000);
 // Railroad crossing tile
 TrainPredictionSignal trainSignal(37, 36, 42, 18, 45, 200, 1000, 5000, 1000, 8, 0, 90);
 SpeedCamera speedCamera(6, 12, 17, 46, 128, 64, 0x3C, LOW, 0.10f, 1.0f, 2000000UL, 500, 2000, "http://192.168.4.1/capture");
-
 
 void setup() {
   pinMode(builtin, OUTPUT);
@@ -37,10 +37,12 @@ void setup() {
   lamp.begin();
   trainSignal.begin();
   speedCamera.begin();  
+  speedCamera.begin();  
 }
 
 void loop() {
   lamp.update();
   trainSignal.update();
+  speedCamera.update();
   speedCamera.update();
 }
