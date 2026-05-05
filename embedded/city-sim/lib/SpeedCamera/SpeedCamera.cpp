db76f7d54d54742521af7b108cde1548c9b33ef9
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2026-05-05 16:47:28 +0200

    feat: :sparkles: Enhance SpeedCamera class with edge detection and ISR handling for improved measurement accuracy

--- embedded/city-sim/lib/SpeedCamera/SpeedCamera.cpp
+++ embedded/city-sim/lib/SpeedCamera/SpeedCamera.cpp
@@ -7,6 +7,8 @@
 #include "NetworkController.h"
 #include "SpeedCameraNetwork.h"
 
+SpeedCamera* SpeedCamera::_instance = NULL;
+
 SpeedCamera::SpeedCamera(int ir1Pin, int ir2Pin, int oledSdaPin, int oledSclPin, int screenWidth,
                          int screenHeight, int oledAddr, int irActiveState, float sensorDistanceM,
                          float speedLimitKmh, unsigned long passTimeoutUs,
@@ -24,11 +26,17 @@ SpeedCamera::SpeedCamera(int ir1Pin, int ir2Pin, int oledSdaPin, int oledSclPin,
       _measureState(IDLE), _firstSensor(0), _tStartUs(0), _lastIr1Active(false),
       _lastIr2Active(false), _lastSpeedKmh(0.0f), _lastTooFast(false), _lastDirection("-"),
       _lastEventMs(0), _lastMeasurementDoneMs(0), _lastUiRefresh(0), _bootScreenStartMs(0),
-      _bootScreenShowing(false) {}
+      _bootScreenShowing(false), _ir1EdgeDetected(false), _ir2EdgeDetected(false),
+      _ir1EdgeTimeUs(0), _ir2EdgeTimeUs(0) {}
 
 void SpeedCamera::begin() {
   pinMode(_ir1Pin, INPUT);
   pinMode(_ir2Pin, INPUT);
+
+  _instance = this;
+
+  attachInterrupt(digitalPinToInterrupt(_ir1Pin), handleIr1ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(_ir2Pin), handleIr2ISR, CHANGE);
 
   _displayWire.begin(_oledSdaPin, _oledSclPin);
 
@@ -48,31 +56,80 @@ void SpeedCamera::begin() {
   Serial.println("Waiting on IR measurements...");
 }
 
+void IRAM_ATTR SpeedCamera::handleIr1ISR() {
+  if (_instance != nullptr) {
+    _instance->handleIrEdge(1);
+  }
+}
+
+void IRAM_ATTR SpeedCamera::handleIr2ISR() {
+  if (_instance != nullptr) {
+    _instance->handleIrEdge(2);
+  }
+}
+
+void IRAM_ATTR SpeedCamera::handleIrEdge(int sensorNumber) {
+  if (sensorNumber == 1) {
+    if (digitalRead(_ir1Pin) == _irActiveState) {
+      _ir1EdgeTimeUs = micros();
+      _ir1EdgeDetected = true;
+    }
+  } else if (sensorNumber == 2) {
+    if (digitalRead(_ir2Pin) == _irActiveState) {
+      _ir2EdgeTimeUs = micros();
+      _ir2EdgeDetected = true;
+    }
+  }
+}
+
 void SpeedCamera::update() {
   bool ir1 = sensorActive(_ir1Pin);
   bool ir2 = sensorActive(_ir2Pin);
+
+  bool edge1 = false;
+  bool edge2 = false;
+  unsigned long edge1TimeUs = 0;
+  unsigned long edge2TimeUs = 0;
+
+  edge1 = _ir1EdgeDetected;
+  edge2 = _ir2EdgeDetected;
+  edge1TimeUs = _ir1EdgeTimeUs;
+  edge2TimeUs = _ir2EdgeTimeUs;
+  _ir1EdgeDetected = false;
+  _ir2EdgeDetected = false;
 
   if (_bootScreenShowing && (millis() - _bootScreenStartMs >= _bootScreenHoldMs)) {
     drawStatusScreen(ir1, ir2);
     _bootScreenShowing = false;
   }
 
-  bool edge1 = ir1 && !_lastIr1Active;
-  bool edge2 = ir2 && !_lastIr2Active;
-
   bool inCooldown = (millis() - _lastMeasurementDoneMs) < _measurementCooldownMs;
 
   switch (_measureState) {
   case IDLE:
     if (!inCooldown) {
-      if (edge1 && !ir2) {
+
+      if (edge1 && edge2) {
+        if (edge1TimeUs < edge2TimeUs) {
+          processMeasurement(1, 2, edge2TimeUs - edge1TimeUs);
+        } else if (edge2TimeUs < edge1TimeUs) {
+          processMeasurement(2, 1, edge1TimeUs - edge2TimeUs);
+        } else {
+          Serial.println("Both sensors triggered at the same time, waiting for clear.");
+          _firstSensor = 0;
+          _tStartUs = 0;
+          _measureState = WAIT_FOR_CLEAR;
+        }
+      }
+
+      else if (edge1 && !ir2) {
         _firstSensor = 1;
-        _tStartUs = micros();
+        _tStartUs = edge1TimeUs;
         _measureState = WAIT_FOR_SECOND_SENSOR;
         Serial.println("Start on IR1");
       } else if (edge2 && !ir1) {
         _firstSensor = 2;
-        _tStartUs = micros();
+        _tStartUs = edge2TimeUs;
         _measureState = WAIT_FOR_SECOND_SENSOR;
         Serial.println("Start on IR2");
       }
@@ -83,15 +140,24 @@ void SpeedCamera::update() {
     unsigned long elapsedUs = micros() - _tStartUs;
 
     if (elapsedUs > _passTimeoutUs) {
-      Serial.println("Timeout, measurement reset.");
-      resetMeasurement();
+      Serial.println("Timeout, waiting until both sensors are clear.");
+      _firstSensor = 0;
+      _tStartUs = 0;
+      _measureState = WAIT_FOR_CLEAR;
     } else if (_firstSensor == 1 && edge2) {
-      processMeasurement(1, 2, elapsedUs);
+      processMeasurement(1, 2, edge2TimeUs - _tStartUs);
     } else if (_firstSensor == 2 && edge1) {
-      processMeasurement(2, 1, elapsedUs);
+      processMeasurement(2, 1, edge1TimeUs - _tStartUs);
     }
     break;
   }
+
+  case WAIT_FOR_CLEAR:
+    if (!ir1 && !ir2) {
+      Serial.println("Both sensors clear, ready for new measurement.");
+      resetMeasurement();
+    }
+    break;
   }
 
   if (!_bootScreenShowing && millis() - _lastUiRefresh > _uiRefreshIntervalMs) {
@@ -289,16 +355,22 @@ void SpeedCamera::triggerCameraOverWiFi() {
 
 void SpeedCamera::processMeasurement(int fromSensor, int toSensor, unsigned long dtUs) {
   if (dtUs == 0) {
-    resetMeasurement();
+    _measureState = WAIT_FOR_CLEAR;
+    _firstSensor = 0;
+    _tStartUs = 0;
     return;
   }
 
   float dtSeconds = dtUs / 1000000.0f;
-  float speedMs = _sensorDistanceM / dtSeconds;
-  float speedKmh = speedMs * 3.6f;
-  bool tooFast = speedKmh > _speedLimitKmh;
 
-  _lastSpeedKmh = speedKmh;
+  float modelSpeedMs = _sensorDistanceM / dtSeconds;
+  float modelSpeedKmh = modelSpeedMs * 3.6f;
+
+  float realSpeedKmh = modelSpeedKmh * Config::SpeedCamera::SCALE_FACTOR;
+
+  bool tooFast = realSpeedKmh > _speedLimitKmh;
+
+  _lastSpeedKmh = realSpeedKmh;
   _lastTooFast = tooFast;
   _lastDirection = (fromSensor == 1 && toSensor == 2) ? "1->2" : "2->1";
   _lastEventMs = millis();
@@ -310,20 +382,26 @@ void SpeedCamera::processMeasurement(int fromSensor, int toSensor, unsigned long
   Serial.println(_lastDirection);
   Serial.print("Time (us): ");
   Serial.println(dtUs);
-  Serial.print("Distance (m): ");
+  Serial.print("Sensor distance model (m): ");
   Serial.println(_sensorDistanceM, 3);
-  Serial.print("Speed (km/u): ");
-  Serial.println(speedKmh, 2);
+  Serial.print("Model speed (km/u): ");
+  Serial.println(modelSpeedKmh, 2);
+  Serial.print("Real speed scale 1:64 (km/u): ");
+  Serial.println(realSpeedKmh, 2);
+  Serial.print("Speed limit real (km/u): ");
+  Serial.println(_speedLimitKmh, 2);
   Serial.print("Too Fast: ");
   Serial.println(tooFast ? "YES" : "NO");
 
-  drawMeasurementScreen(speedKmh, tooFast, _lastDirection, dtUs);
+  drawMeasurementScreen(realSpeedKmh, tooFast, _lastDirection, dtUs);
 
-  SpeedCameraNetwork::sendMeasurement(speedKmh, _lastDirection, tooFast, _speedLimitKmh);
+  SpeedCameraNetwork::sendMeasurement(realSpeedKmh, _lastDirection, tooFast, _speedLimitKmh);
 
   if (tooFast) {
     triggerCameraOverWiFi();
   }
 
-  resetMeasurement();
+  _measureState = WAIT_FOR_CLEAR;
+  _firstSensor = 0;
+  _tStartUs = 0;
 }
